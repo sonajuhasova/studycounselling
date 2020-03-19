7b8cf6870c4feabaffb243e6a0a9514de47b3b68
Sona <sona.juhasova@gmail.com>
2020-03-19 16:34:01 +0100

    popups almost work

---
+++ script.js
@@ -0,0 +1,76 @@
+//what is study counselling popup
+var popup1 = document.getElementById("what-is-sc");
+var btn = document.getElementById("what-btn");
+var goBack = document.getElementsByClassName("go-back")[0];
+btn.onclick = function () {
+    popup1.style.display = "block";
+}
+goBack.onclick = function () {
+    popup1.style.display = "none";
+}
+
+// faq popup
+var popup2 = document.getElementById("faq");
+var btn = document.getElementById("faq-btn");
+var goBack = document.getElementsByClassName("go-back")[0];
+btn.onclick = function () {
+    popup2.style.display = "block";
+}
+goBack.onclick = function () {
+    popup2.style.display = "none";
+}
+
+// interviews popup
+var popup3 = document.getElementById("interviews");
+var btn = document.getElementById("interviews-btn");
+var goBack = document.getElementsByClassName("go-back")[0];
+btn.onclick = function () {
+    popup3.style.display = "block";
+}
+goBack.onclick = function () {
+    popup3.style.display = "none";
+}
+
+// ask a question popup
+var popup4 = document.getElementById("ask");
+var btn = document.getElementById("ask-btn");
+var goBack = document.getElementsByClassName("go-back")[0];
+btn.onclick = function () {
+    popup4.style.display = "block";
+}
+goBack.onclick = function () {
+    popup4.style.display = "none";
+}
+
+// contact popup
+var popup5 = document.getElementById("contact");
+var btn = document.getElementById("contact-btn");
+var goBack = document.getElementsByClassName("go-back")[0];
+btn.onclick = function () {
+    popup5.style.display = "block";
+}
+goBack.onclick = function () {
+    popup5.style.display = "none";
+}
+
+// about programmes popup
+var popup6 = document.getElementById("about");
+var btn = document.getElementById("about-btn");
+var goBack = document.getElementsByClassName("go-back")[0];
+btn.onclick = function () {
+    popup6.style.display = "block";
+}
+goBack.onclick = function () {
+    popup6.style.display = "none";
+}
+
+// faq application popup
+var popup7 = document.getElementById("faq-a");
+var btn = document.getElementById("faq-a-btn");
+var goBack = document.getElementsByClassName("go-back")[0];
+btn.onclick = function () {
+    popup7.style.display = "block";
+}
+goBack.onclick = function () {
+    popup7.style.display = "none";
+}
